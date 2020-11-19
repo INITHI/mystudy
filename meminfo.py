eb4bec9c412883893f4228d24652e74162227c48
INITHI <38148868+INITHI@users.noreply.github.com>
2020-11-19 20:47:50 +0900

    Update meminfo.py
    packagename argument로 입력받을수있도록 적용
    입력안하면 코드에서 수정하여야함

--- meminfo.py
+++ meminfo.py
@@ -1,4 +1,4 @@
-﻿# This Python file uses the following encoding: utf-8
+# This Python file uses the following encoding: utf-8
 
 import os
 import platform
@@ -19,45 +19,62 @@ dir(os)
 # 현재 디렉토리
 current_path = os.getcwd()
 filename = 'resultMeminfo.txt'
+
+
 def adbmeminfo():
+    current_time = time.localtime()
+    # adb 실행만 subprocess.call을 이용하면 다른 command도 사용가능할것으로 보임
+    # retCode = subprocess.call(['adb','shell','dumpsys','meminfo'])
+    # adb 실행 and 결과물 return
+    packagename = checkPackageName()
+    if not packagename:
+        result = subprocess.check_output(['adb', 'shell', 'dumpsys', 'meminfo', 'com.study'])
+    else:
+        result = subprocess.check_output(['adb', 'shell', 'dumpsys', 'meminfo', packagename])
 
-	current_time = time.localtime()
-	# adb 실행만 subprocess.call을 이용하면 다른 command도 사용가능할것으로 보임
-	#retCode = subprocess.call(['adb','shell','dumpsys','meminfo'])
-	# adb 실행 and 결과물 return
-	result = subprocess.check_output(['adb','shell','dumpsys','meminfo','com.study'])
-
-#	print (result)
-	writeResult(result)
+    if 'No process found for' in str(result):
+        print (result)
+        return 0
+    else:
+        writeResult(result)
+        #print (result)
+        return 1
 
 
 def writeResult(result):
+    resultfile = open(current_path + '/' + filename, 'ab')
+    current_time = time.localtime()
 
-	resultfile = open(current_path + '/' + filename, 'ab')
-	current_time = time.localtime()
+    resultfile.write('=====================================================================================\n'.encode())
+    year, month, mday, hour, min, sec, wday, yday, daylight = current_time
+    current_date = '%04d/%02d/%02d' % (current_time.tm_year, current_time.tm_mon, current_time.tm_mday)
+    current_time = '%02d:%02d:%02d' % (current_time.tm_hour, current_time.tm_min, current_time.tm_sec)
+    date_time = current_date + ' ' + current_time + '\n'
+    print (date_time)
+    resultfile.write(date_time.encode())
+    resultfile.write(result)
+    resultfile.write('\n'.encode())
+    resultfile.close()
 
-	resultfile.write('=====================================================================================\n'.encode())
-	year, month, mday, hour, min, sec, wday, yday, daylight = current_time
-	current_date = '%04d/%02d/%02d' % (current_time.tm_year, current_time.tm_mon, current_time.tm_mday)
-	current_time = '%02d:%02d:%02d' % (current_time.tm_hour, current_time.tm_min, current_time.tm_sec)
-	date_time = current_date + ' ' + current_time + '\n'
-	print (date_time)
-	resultfile.write(date_time.encode())
-	resultfile.write(result)
-	resultfile.write('\n'.encode())
-	resultfile.close()
 
 def startMemCheck():
+    s = sched.scheduler(time.time, time.sleep)
 
-	s = sched.scheduler(time.time, time.sleep)
+    def do_something(sc):
+        if adbmeminfo() == 1:
+            s.enter(10, 0, do_something, (sc,))
 
-	def do_something(sc):
-		adbmeminfo()
-		s.enter(10, 0, do_something, (sc,))
+    # 초 단위임 1이 1초 millisecond 아님 주의
+    s.enter(0, 0, do_something, (s,))
+    s.run()
 
-	# 초 단위임 1이 1초 millisecond 아님 주의
-	s.enter(0, 0, do_something, (s,))
-	s.run()
+
+def checkPackageName():
+    argumentlen = len(sys.argv)
+    if argumentlen >= 2:
+        return sys.argv.__getitem__(1)
+
+    return ''
+
 
 startMemCheck()
-
